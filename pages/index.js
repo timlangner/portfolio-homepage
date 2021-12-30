7bb3b9609e8a02fca4d81db5de323c384cca6f8f
Tim <tim.langner0@gmail.com>
2021-12-30 21:17:20 +0100

    Integrate Chakra UI

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
 const Page = () => {
-    return <div>Hello</div>
+    return (
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} align="center">
+                Hello, I&apos;m a full-stack developer based in Germany!
+            </Box>
+
+            <Box display={{md: 'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Tim Langner
+                    </Heading>
+                    <p>Developer / Designer</p>
+                </Box>
+            </Box>
+        </Container>
+    )
 }
 
 export default Page
