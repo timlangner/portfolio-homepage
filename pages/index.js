5df2444b154bf93d706071f3d51bc221393044b2
Tim <tim.langner0@gmail.com>
2021-12-31 21:41:44 +0100

    Add profile Picture

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="red" p={3} align="center">
+            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} align="center">
                 Hello, I&apos;m a full-stack developer based in Germany!
             </Box>
 
@@ -13,6 +13,23 @@ const Page = () => {
                         Tim Langner
                     </Heading>
                     <p>Developer / Designer</p>
+                </Box>
+                <Box
+                    flexShrink={0}
+                    mt={{ base: 4, md: 0 }}
+                    ml={{ md: 6 }}
+                    textAlign="center"
+                >
+                <Image
+                    borderColor="whiteAlpha.800"
+                    borderWidth={2}
+                    borderStyle="solid"
+                    maxWidth="100px"
+                    display="inline-block"
+                    borderRadius="full"
+                    src="/images/profilePicture.jpg"
+                    alt="Profile image"
+                />
                 </Box>
             </Box>
         </Container>
