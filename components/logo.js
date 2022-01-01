8bdae20e043026fc537ec1d52c1cf5bde6d8b80c
Tim <tim.langner0@gmail.com>
2022-01-01 17:38:22 +0100

    Remove footprint next to logo

--- components/logo.js
+++ components/logo.js
@@ -20,13 +20,11 @@ const LogoBox = styled.span`
 `
 
 const Logo = () => {
-  const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
 
   return (
     <Link href="/">
       <a>
         <LogoBox>
-          <Image src={footPrintImg} width={20} height={20} alt="logo" />
           <Text
             color={useColorModeValue('gray.800', 'whiteAlpha.900')}
             fontFamily='M PLUS Rounded 1c", sans-serif'
