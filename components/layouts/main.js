8eb8179a140157ca1e65cc4e959e1d1dca650a5c
Tim <tim.langner0@gmail.com>
2022-01-01 23:51:10 +0100

    Add 3D voxel art

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,18 +1,30 @@
 import Head from 'next/head'
+import dynamic from 'next/dynamic'
 import Navbar from '../navbar.js'
 import { Box, Container } from '@chakra-ui/react'
+import VoxelDogLoader from '../voxel-dog-loader'
+
+const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
+  ssr: false,
+  loading: () => <VoxelDogLoader />
+})
 
 const Main = ({ children, router }) => {
     return (
         <Box as="main" pb={8}>
             <Head>
                 <meta name="viewport" content="width=device-width , initial-scale=1" />
+                <meta name="description" content="Tim's homepage" />
+                <meta name="author" content="Tim Langner" />
+                <meta property="og:site_name" content="Tim's homepage" />
+                <meta property="og:type" content="website" />
                 <title>Tim Langner - Homepage</title>
             </Head>
 
             <Navbar path={router.asPath} />
 
-            <Container max="container.md" pt={14}>
+            <Container maxW="container.md" pt={14}>
+                <LazyVoxelDog />
                 {children}
             </Container>
         </Box>
