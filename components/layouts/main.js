024af8196b2ae9315c5b8522bf1f8ee3984eb358
Tim <tim.langner0@gmail.com>
2022-01-02 14:18:18 +0100

    Update description meta tag

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -14,9 +14,9 @@ const Main = ({ children, router }) => {
         <Box as="main" pb={8}>
             <Head>
                 <meta name="viewport" content="width=device-width , initial-scale=1" />
-                <meta name="description" content="Tim's homepage" />
+                <meta name="description" content="Tim is an iOS developer who currently works on multiple iOS Apps within a team of six engineers." />
                 <meta name="author" content="Tim Langner" />
-                <meta property="og:site_name" content="Tim's homepage" />
+                <meta property="og:site_name" content="Tim is an iOS developer who currently works on multiple iOS Apps within a team of six engineers." />
                 <meta property="og:type" content="website" />
                 <title>Tim Langner - Homepage</title>
             </Head>
