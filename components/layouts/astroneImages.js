47e39f0e2ad9332b3bf73e62aa394f4aa4fc0348
Tim Langner <tim.langner0@gmail.com>
2023-02-10 16:05:39 +0100

    Show images next to each other when enough screen space is available

---
+++ components/layouts/astroneImages.js
@@ -0,0 +1,36 @@
+import { Image } from "@chakra-ui/react";
+
+const AstroneImages = () => (
+  <>
+    <Image
+      borderRadius="lg"
+      w="100%"
+      src="/images/projects/DiscoverPage.png"
+      alt="Discover Page"
+      mb={4}
+    />
+    <Image
+      borderRadius="lg"
+      w="100%"
+      src="/images/projects/EventDetails.png"
+      alt="Event Details"
+      mb={4}
+    />
+    <Image
+      borderRadius="lg"
+      w="100%"
+      src="/images/projects/News.png"
+      alt="News"
+      mb={4}
+    />
+    <Image
+      borderRadius="lg"
+      w="100%"
+      src="/images/projects/Reminders.png"
+      alt="Reminders"
+      mb={4}
+    />
+  </>
+);
+
+export default AstroneImages;
--- pages/projects/astrone.js
+++ pages/projects/astrone.js
@@ -1,12 +1,16 @@
 import {
   Container,
+  Flex,
+  Hide,
   Image,
   Link,
   List,
-  ListItem
+  ListItem,
+  Show
 } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, Meta } from '../../components/project'
+import AstroneImages from '../../components/layouts/astroneImages'
 import Layout from '../../components/layouts/article'
 
 const Project = () => (
@@ -15,8 +19,7 @@ const Project = () => (
       <Title>Astrone</Title>
       <p>
         Astrone is your personal guide for all kinds of exciting astronomy
-        events.
-        Whether you just quickly want to explore any upcoming events or
+        events. Whether you just quickly want to explore any upcoming events or
         want to know when the next solar eclipse is happining, you&apos;re in
         the right place. Simply check out our Discover page and find yourself
         something you like.
@@ -46,34 +49,43 @@ const Project = () => (
         </ListItem>
       </List>
 
-      <Image
-        borderRadius="lg"
-        w="50%"
-        src="/images/projects/DiscoverPage.png"
-        alt="Discover Page"
-        mb={4}
-      />
-      <Image
-        borderRadius="lg"
-        w="50%"
-        src="/images/projects/EventDetails.png"
-        alt="Event Details"
-        mb={4}
-      />
-      <Image
-        borderRadius="lg"
-        w="50%"
-        src="/images/projects/News.png"
-        alt="News"
-        mb={4}
-      />
-      <Image
-        borderRadius="lg"
-        w="50%"
-        src="/images/projects/Reminders.png"
-        alt="Reminders"
-        mb={4}
-      />
+      <Show above="md">
+        <AstroneImages />
+      </Show>
+      <Hide below="md">
+        <Flex align="center" gap={3}>
+          <Image
+            borderRadius="lg"
+            w="50%"
+            src="/images/projects/DiscoverPage.png"
+            alt="Discover Page"
+            mb={4}
+          />
+          <Image
+            borderRadius="lg"
+            w="50%"
+            src="/images/projects/EventDetails.png"
+            alt="Event Details"
+            mb={4}
+          />
+        </Flex>
+        <Flex align="center" gap={3}>
+          <Image
+            borderRadius="lg"
+            w="50%"
+            src="/images/projects/News.png"
+            alt="News"
+            mb={4}
+          />
+          <Image
+            borderRadius="lg"
+            w="50%"
+            src="/images/projects/Reminders.png"
+            alt="Reminders"
+            mb={4}
+          />
+        </Flex>
+      </Hide>
     </Container>
   </Layout>
 )
